b0f546457d6eb817ceb46544f4ea5ec45abb8a78
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-03-24 14:13:14 -0400

    Test for 1 created, list function created, test failing

---
+++ Prime Factors/src/primeFactors/PrimeFactorsTest.java
@@ -0,0 +1,21 @@
+package primeFactors;
+
+import static org.junit.Assert.*;
+
+import java.awt.List;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class PrimeFactorsTest {
+
+	@Test
+	public void testOne() {
+		assertEquals(list(), PrimeFactors.generate(1));
+	}
+
+	private java.util.List<Integer> list() {
+		return Arrays.asList();
+	}
+
+}
