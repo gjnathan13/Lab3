b71831d1d0b4c35f8c2011a50d935bbf124c7f4a
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-03-24 14:15:53 -0400

    PrimeFactors class created,  generate method created, simplest code implemented to pass test for 1

---
+++ Prime Factors/src/primeFactors/PrimeFactors.java
@@ -0,0 +1,12 @@
+package primeFactors;
+
+import java.util.ArrayList;
+
+public class PrimeFactors {
+
+	public static java.util.List<Integer> generate (int i) {
+		return new ArrayList<Integer>();
+	}
+
+	
+}
