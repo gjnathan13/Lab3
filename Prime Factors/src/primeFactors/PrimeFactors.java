3efd24aadce0352b28692ab5f68df1dbd56c6c56
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-03-24 14:21:31 -0400

    Simple code created to pass 2 test case

--- Prime Factors/src/primeFactors/PrimeFactors.java
+++ Prime Factors/src/primeFactors/PrimeFactors.java
@@ -4,8 +4,22 @@ import java.util.ArrayList;
 
 public class PrimeFactors {
 
+	/**
+	 * Creates prime factor list for input integer
+	 * 
+	 * @param i
+	 * @return
+	 */
 	public static java.util.List<Integer> generate (int i) {
-		return new ArrayList<Integer>();
+		if(i == 1){
+			return new ArrayList<Integer>();
+		}
+		else{
+			ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+			primeFactors.add(2);
+			return primeFactors;
+		}
+		
 	}
 
 	
