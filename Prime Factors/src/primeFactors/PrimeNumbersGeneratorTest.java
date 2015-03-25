0b3cc9de125879511c30ec1140d269722105cbcf
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-03-24 20:55:28 -0400

    Parametrized testing set up, test created for 2, fails due to no class existing

---
+++ Prime Factors/src/primeFactors/PrimeNumbersGeneratorTest.java
@@ -0,0 +1,44 @@
+package primeFactors;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+
+@RunWith(Parameterized.class)
+public class PrimeNumbersGeneratorTest {
+	
+	private Integer inputNumber;
+	private java.util.List<Integer> expectedResult;
+	
+	
+	public PrimeNumbersGeneratorTest(Integer inputNumber, 
+			java.util.List<Integer> expectedResult) {
+		      this.inputNumber = inputNumber;
+		      this.expectedResult = expectedResult;
+		   }
+
+	@Parameterized.Parameters
+	public static Collection primeNumbersChecks(){
+		return Arrays.asList(new Object[][]{
+				{2, list()},
+				});
+	}
+	
+	private static java.util.List<Integer> list(Integer...integers) {
+		return Arrays.asList(integers);
+	}
+
+	
+	@Test
+	public void testPrimeNumberGeneration() {
+		assertEquals(expectedResult, PrimeNumberGeneration.generateSmallerPrimes(inputNumber));
+	}
+
+}
