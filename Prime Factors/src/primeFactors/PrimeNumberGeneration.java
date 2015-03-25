09fb810763bfcce422995a4bbc9aad0ba133370e
nathangj <nathangj@NATHANGJ-1.rose-hulman.edu>
2015-03-24 20:57:32 -0400

    PrimeNumberGenerator class created, simple code input to pass test for 2

---
+++ Prime Factors/src/primeFactors/PrimeNumberGeneration.java
@@ -0,0 +1,12 @@
+package primeFactors;
+
+import java.util.ArrayList;
+
+public class PrimeNumberGeneration {
+
+	public static java.util.List<Integer> generateSmallerPrimes(int inputNumber) {
+		return new ArrayList<Integer>();
+	}
+
+	
+}
